f43bec1d8e81a0b63f7a54d07ca46e09b8c7e109
elegin <elegin@gmail.com>
2020-12-02 16:24:23 -0500

    added meme command

--- bot.js
+++ bot.js
@@ -1,21 +1,34 @@
 require('dotenv').config();
+const axios = require('axios');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = "!"
 
 client.on('ready', () => {
     console.log(`Im live, maybe logged in as ${client.user.tag}!`);
-});
+  });
 
 client.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const command = args.shift().toLowerCase();
-    message.channel.send(`Command name: ${command}\nArguments: ${args}`);
 
     if (message.content ===`${prefix}ping`) {
-        message.channel.send('Pong!');
+        message.channel.send('Pong!!!!!!!!!!!');
     }
+    
+    if (message.content ===`${prefix}meme`) {
+        axios.get('https://meme-api.herokuapp.com/gimme')
+        .then(response => {
+          console.log(response.data.url);
+          const embed = new Discord.MessageEmbed()
+          .setImage(response.data.url)
+        message.channel.send(embed);         
+        })
+        .catch(error => {
+          console.log(error);
+        });
+     
+    }
+
 });
 
 client.login(process.env.BOT_TOKEN);
