cf4f8fb30bb89b17645087361a776133e2b15585
Aaron Steimle <aarons@glyphip.com>
2020-12-02 20:57:15 -0500

    maybe fixed

--- bot.js
+++ bot.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const axios = require('axios');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = "!"
+const prefix = "!";
+const cmds = ["!meme","!russ","!elegin"];
+const types = {"!meme":"dankmemes","!russ":"oddlysatisfying","!elegin":"vegan"};
 
 client.on('ready', () => {
     console.log(`Im live, maybe logged in as ${client.user.tag}!`);
@@ -11,10 +13,10 @@ client.on('ready', () => {
 client.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
     
-    if (message.content ===`${prefix}meme`) {
+    if (cmds.includes(message.content)) {
         const args = message.content.slice(prefix.length).trim().split(/ +/);
         // const command = args.shift().toLowerCase();
-        axios.get(`https://meme-api.herokuapp.com/gimme/dankmemes`)
+        axios.get(`https://meme-api.herokuapp.com/gimme/${types[message.content]}`)
         .then(response => {
           console.log(response.data.url);
           const embed = new Discord.MessageEmbed()
@@ -27,45 +29,46 @@ client.on('message', message => {
         });
 
     }
-    if (message.content ===`${prefix}elegin`) {
-        axios.get(`https://meme-api.herokuapp.com/gimme/vegan`)
-        .then(response => {
-          console.log(response.data.url);
-          const embed = new Discord.MessageEmbed()
-          .setImage(response.data.url)
-        message.channel.send(embed);  
-        return;       
-        })
-        .catch(error => {
-          console.log(error);
-        });
+    if (!cmds.includes(message.content) {
+      axios.get(`https://meme-api.herokuapp.com/gimme/reactiongifs`)
+      .then(response => {
+        console.log(response.data.url);
+        const embed = new Discord.MessageEmbed()
+        .setImage(response.data.url)
+      message.channel.send(embed);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    }
+    // if (cmds.includes(message.content)) {
+    //     axios.get(`https://meme-api.herokuapp.com/gimme/vegan`)
+    //     .then(response => {
+    //       console.log(response.data.url);
+    //       const embed = new Discord.MessageEmbed()
+    //       .setImage(response.data.url)
+    //     message.channel.send(embed);  
+    //     return;       
+    //     })
+    //     .catch(error => {
+    //       console.log(error);
+    //     });
      
-    }
-    if (message.content ===`${prefix}russ`) {
-        axios.get(`https://meme-api.herokuapp.com/gimme/oddlysatisfying`)
-        .then(response => {
-          console.log(response.data.url);
-          const embed = new Discord.MessageEmbed()
-          .setImage(response.data.url)
-        message.channel.send(embed);
-        return;         
-        })
-        .catch(error => {
-          console.log(error);
-        });
+    // }
+    // if (cmds.includes(message.content) {
+    //     axios.get(`https://meme-api.herokuapp.com/gimme/oddlysatisfying`)
+    //     .then(response => {
+    //       console.log(response.data.url);
+    //       const embed = new Discord.MessageEmbed()
+    //       .setImage(response.data.url)
+    //     message.channel.send(embed);
+    //     return;         
+    //     })
+    //     .catch(error => {
+    //       console.log(error);
+    //     });
     
-    }
-
-  axios.get(`https://meme-api.herokuapp.com/gimme/reactiongifs`)
-  .then(response => {
-    console.log(response.data.url);
-    const embed = new Discord.MessageEmbed()
-    .setImage(response.data.url)
-  message.channel.send(embed);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+    // }
 
 
 });
